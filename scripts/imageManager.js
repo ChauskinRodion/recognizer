2b093c61ca33a6c4cf971977627c88b453a5478b
xixi4Den <xixi4.den@gmail.com>
2015-09-26 01:22:21 +0300

    implemented getRgbMap

--- scripts/imageManager.js
+++ scripts/imageManager.js
@@ -1,30 +1,61 @@
 ﻿var ImageRecognitionLab = ImageRecognitionLab || {
 
     TargetImage: function (src) {
-        //private members
-        var pixels = new Array();
-        var image = new Image();
-        image.onload = function () {
+        var self = this;
+
+        self.src = src;
+        self.rgbMap = null;
+
+        function initialize() {
+            var deferred = $.Deferred();
+            var image = new Image();
+            image.onload = function () {
+                self.rgbMap = getRgbMapFromImage(image);
+                deferred.resolve();
+            }
+            image.onerror = function () {
+                deferred.reject();
+            }
+            image.src = self.src;
+            return deferred.promise();
+        }
+
+        function createCanvas(image) {
             var canvas = document.createElement('canvas');
             canvas.width = image.width;
             canvas.height = image.height;
+            return canvas;
+        }
+
+        function getImageData(canvas, image) {
             var context = canvas.getContext('2d');
             context.drawImage(image, 0, 0);
-            var imageData = context.getImageData(0, 0, image.width, image.height);
+            return context.getImageData(0, 0, image.width, image.height);
+        }
+
+        function getRgbMap(canvas, imageData) {
+            var pixels = new Array();
             for (i = 0; i < canvas.height; i++) {
                 pixels[i] = new Array();
                 for (j = 0; j < canvas.width; j++) {
                     pixels[i][j] = imageData.data[i * canvas.width + j * 4];
                 }
             }
+            return pixels;
         }
-        image.src = src;        
 
-        //public members
+        function getRgbMapFromImage(image) {
+            var canvas = createCanvas(image);
+            var imageData = getImageData(canvas, image);
+            return getRgbMap(canvas, imageData);
+        }
+
         return {
-            getImageData: function(){
-                return pixels;
-            }
+            getRgbMap: function () {
+                return self.rgbMap;
+            },
+
+            initialize: initialize,
         }
     }
 };
