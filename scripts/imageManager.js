89863c8b1f5ec32641e1df56294096a12a3f0da2
xixi4Den <xixi4.den@gmail.com>
2015-09-26 02:14:07 +0300

    Encapsulate RgbMap

--- scripts/imageManager.js
+++ scripts/imageManager.js
@@ -1,61 +1,49 @@
-﻿var ImageRecognitionLab = ImageRecognitionLab || {
+﻿var ImageRecognitionLab = ImageRecognitionLab || {};
 
-    TargetImage: function (src) {
-        var self = this;
+ImageRecognitionLab.TargetImage = function (src) {
+    var self = this;
 
-        self.src = src;
-        self.rgbMap = null;
+    self.src = src;
+    self.rgbMap = null;
 
-        function initialize() {
-            var deferred = $.Deferred();
-            var image = new Image();
-            image.onload = function () {
-                self.rgbMap = getRgbMapFromImage(image);
-                deferred.resolve();
-            }
-            image.onerror = function () {
-                deferred.reject();
-            }
-            image.src = self.src;
-            return deferred.promise();
+    function initialize() {
+        var deferred = $.Deferred();
+        var image = new Image();
+        image.onload = function () {
+            self.rgbMap = getRgbMapFromImage(image);
+            deferred.resolve();
         }
-
-        function createCanvas(image) {
-            var canvas = document.createElement('canvas');
-            canvas.width = image.width;
-            canvas.height = image.height;
-            return canvas;
+        image.onerror = function () {
+            deferred.reject();
         }
-
-        function getImageData(canvas, image) {
-            var context = canvas.getContext('2d');
-            context.drawImage(image, 0, 0);
-            return context.getImageData(0, 0, image.width, image.height);
-        }
-
-        function getRgbMap(canvas, imageData) {
-            var pixels = new Array();
-            for (i = 0; i < canvas.height; i++) {
-                pixels[i] = new Array();
-                for (j = 0; j < canvas.width; j++) {
-                    pixels[i][j] = imageData.data[i * canvas.width + j * 4];
-                }
-            }
-            return pixels;
-        }
-
-        function getRgbMapFromImage(image) {
-            var canvas = createCanvas(image);
-            var imageData = getImageData(canvas, image);
-            return getRgbMap(canvas, imageData);
-        }
-
-        return {
-            getRgbMap: function () {
-                return self.rgbMap;
-            },
-
-            initialize: initialize,
-        }
+        image.src = self.src;
+        return deferred.promise();
     }
-};
+
+    function createCanvas(image) {
+        var canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        return canvas;
+    }
+
+    function getImageData(canvas, image) {
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+        return context.getImageData(0, 0, image.width, image.height);
+    }
+
+    function getRgbMapFromImage(image) {
+        var canvas = createCanvas(image);
+        var imageData = getImageData(canvas, image);
+        return new ImageRecognitionLab.RgbMap(canvas.width, canvas.height, imageData);
+    }
+
+    return {
+        getRgbMap: function () {
+            return self.rgbMap;
+        },
+
+        initialize: initialize,
+    }
+}
---
+++ scripts/rgbMap.js
@@ -0,0 +1,56 @@
+﻿var ImageRecognitionLab = ImageRecognitionLab || {}
+    
+ImageRecognitionLab.RgbMap = function (width, height, imageData) {
+    var self = this;
+    self.pixels = createRgbMap(width, height, imageData);
+
+    function createRgbMap(width, height, imageData) {
+        var pixels = new Array();
+        for (i = 0; i < height; i++) {
+            pixels[i] = new Array();
+            for (j = 0; j < width; j++) {
+                pixels[i][j] = new Array();
+                var index = j * 4;
+                pixels[i][j][0] = imageData.data[i * width + index];
+                index++;
+                pixels[i][j][1] = imageData.data[i * width + index];
+                index++;
+                pixels[i][j][2] = imageData.data[i * width + index];
+            }
+        }
+        return pixels;
+    }
+
+    function getRed(i, j) {
+        return self.pixels[i][j][0];
+    }
+
+    function getGreen(i, j) {
+        return self.pixels[i][j][1];
+    }
+
+    function getBlue(i, j) {
+        return self.pixels[i][j][2];
+    }
+
+    function setRed(i, j, value) {
+        self.pixels[i][j][0] = value;
+    }
+
+    function setGreen(i, j, value) {
+        self.pixels[i][j][1] = value;
+    }
+
+    function setBlue(i, j, value) {
+        self.pixels[i][j][2] = value;
+    }
+
+    return {
+        getRed: getRed,
+        getGreen: getGreen,
+        getBlue: getBlue,
+        setRed: setRed,
+        setGreen: setGreen,
+        setBlue: setBlue,
+    }
+}
